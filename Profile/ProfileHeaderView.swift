f9c960b24a09a60d6e2a4a7e55672d6cb2c0cb20
Oleg <voisemail@mail.ru>
2026-03-30 00:05:41 +0300

    Finish ProfileHeaderView with correct layout, cat image and button

---
+++ Profile/ProfileHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Oleg Stepanov on 29.03.2026.
+//
+
+import Foundation
---
+++ Profile/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Oleg Stepanov on 29.03.2026.
+//
+
+import Foundation
