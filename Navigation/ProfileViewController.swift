0c207ac80c2d784af387fea4c6d27ee901b5152f
Oleg <voisemail@mail.ru>
2026-03-29 09:43:39 +0300

    Initial commit for develop-iosui branch

---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Oleg Stepanov on 22.03.2026.
+//
+
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGroupedBackground
+        title = "Профиль"
+    }
+}
